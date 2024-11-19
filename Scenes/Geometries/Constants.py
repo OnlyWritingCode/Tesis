3d57e0e7147016e5dd9ccf960097d313f6918e8d
OnlyWrittingCode <bayron2033@gmail.com>
2024-11-19 18:41:33 -0300

    Commit de semi-garra total con espacio en el centro funcionando en SOFA

--- Scenes/Geometries/Constants.py
+++ Scenes/Geometries/Constants.py
@@ -1,12 +1,12 @@
 import numpy as np
 GripperHeight = 100
-Depth = 10
+Depth = 8
 GripperWidth = 30
 Theta = np.arctan2(GripperHeight, GripperWidth)
 Phi = np.arctan2(GripperWidth, GripperHeight)
 GripperHeightGift = 2
 
-WallThickness = 3 
+WallThickness = 1.5 
 
 
 BarHeightThick = 2
@@ -20,4 +20,4 @@ NColsTactile = 4
 
 #lO NUEVO 
 
-DepthOuter = 13  # Profundidad de la pared externa
+DepthOuter = 10  # Profundidad de la pared externa
